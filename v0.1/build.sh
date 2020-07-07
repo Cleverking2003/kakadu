33fd8bee8d7a766e6f93feb00a5bd1595a3ca7c6
Begggs <begggsss@protonmail.com>
2020-07-07 22:28:01 +0300

    Removed build script

--- v0.1/build.sh
+++
@@ -1,3 +0,0 @@
-nasm -f elf32 source/kernel.asm -o bin/building/kasm.o
-gcc -fno-stack-protector -m32 -c source/kernel.c -o bin/building/kc.o
-ld -m elf_i386 -T source/link.ld -o kernel bin/building/kasm.o bin/building/kc.o
